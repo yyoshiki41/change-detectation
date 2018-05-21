e6293631d853262dc8079580471b0b3564423c73
yyoshiki41 <yyoshiki41@gmail.com>
2018-05-22 04:12:55 +0900

    fix(codedeploy): fixed

--- scripts/codedeploy-ruby-v25.sh
+++ scripts/codedeploy-ruby-v25.sh
@@ -5,7 +5,7 @@ set -e
 wget https://aws-codedeploy-ap-northeast-1.s3.amazonaws.com/latest/install
 resp=$(grep -A 2 -r "def supported_ruby_versions" install)
 
-if echo "${resp}" | grep -q "2.4" ; then
+if echo "${resp}" | grep -q "2.5" ; then
   echo "codedeploy agent supports ruby v2.5" | ruby reporter.rb
 fi
 
